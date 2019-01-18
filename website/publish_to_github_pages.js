eb72dc133f385f1b0fdc5f98292aa36b673533a1
Kasra Bigdeli <kasra85@gmail.com>
2019-01-17 23:54:57 -0800

    Added custom publish script for github pages.

---
+++ website/publish_to_github_pages.js
@@ -0,0 +1,11 @@
+const ghpages = require('gh-pages');
+const path = require('path');
+const fs = require('fs-extra')
+
+
+ghpages.publish('build/caprover', function (err) {
+    if (err)
+        console.log(err);
+    else
+        console.log('Built and deployed successfully');
+});
