b2489f73e1b2944c2834914eb8f9105a552ea773
Kasra Bigdeli <kasra85@gmail.com>
2026-08-15 21:58:57 -0700

    Make sitemap compatible with static export

--- homepage/app/sitemap.ts
+++ homepage/app/sitemap.ts
@@ -1,5 +1,7 @@
 import type { MetadataRoute } from "next";
 
+export const dynamic = "force-static";
+
 export default function sitemap(): MetadataRoute.Sitemap {
   const base = "https://caprover.com";
   const lastModified = new Date("2026-08-15");
